3470862aade54b497a2572d76277edbff5b41edb
Adrian Spinelli <adrijk7.cr@gmail.com>
2026-03-26 09:09:25 -0300

    fix: make web app standalone for Vercel deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@labelflow/shared'],
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'bullmq', 'ioredis'],
   },
